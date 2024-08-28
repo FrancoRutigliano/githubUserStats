a8241509f08d8d472dc5b701b1dfe6c64bd1456a
Franco Rutigliano <francorutigliano11@gmail.com>
2024-08-28 16:15:56 -0300

    re order of the project structure

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module francorutigliano/githubstats
 
 go 1.23.0
 
-require (
-	github.com/a-h/templ v0.2.771
-	github.com/joho/godotenv v1.5.1
-)
+require github.com/joho/godotenv v1.5.1
--- internal/controllers/search.go
+++ internal/controllers/search.go
@@ -1,15 +1,9 @@
 package controllers
 
 import (
-	"francorutigliano/githubstats/internal/templates"
 	"net/http"
 )
 
 func Search(w http.ResponseWriter, r *http.Request) {
-	c := templates.Search()
 
-	err := templates.Layout(c, "search user").Render(r.Context(), w)
-	if err != nil {
-		http.Error(w, "error rendering template", http.StatusInternalServerError)
-	}
 }
